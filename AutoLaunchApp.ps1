531e5ad55a320a46ce70d375661cb0bee4863a23
get543 <fghaza.pratama@gmail.com>
2024-10-11 19:30:32 +0700

    add autolaunch application script

---
+++ AutoLaunchApp.ps1
@@ -0,0 +1,17 @@
+# Launch Spotify
+& "${env:APPDATA}\Spotify\Spotify.exe"
+
+# Launch Telegram
+& "${env:APPDATA}\Telegram Desktop\Telegram.exe"
+
+# Launch Firefox
+& "${env:ProgramFiles}\Mozilla Firefox\firefox.exe"
+
+# Launch Obsidian
+& "${env:LOCALAPPDATA}\Programs\obsidian\Obsidian.exe"
+
+# Launch UWP Apps (WhatsApp)
+explorer.exe shell:appsFolder\5319275A.WhatsAppDesktop_cv1g1gvanyjgm!App
+
+# Launch Discord
+& "${env:LOCALAPPDATA}\Discord\Update.exe" --processStart Discord.exe
