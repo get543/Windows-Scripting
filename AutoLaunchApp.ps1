d053772af4ecd1d0138108e19cc1d515b56afcbe
get543 <fghaza.pratama@gmail.com>
2025-10-02 13:16:49 +0700

    launch obsidian using cmd command to detach from terminal

--- AutoLaunchApp.ps1
+++ AutoLaunchApp.ps1
@@ -8,7 +8,7 @@
 & "${env:ProgramFiles}\Mozilla Firefox\firefox.exe"
 
 # Launch Obsidian
-& "${env:LOCALAPPDATA}\Programs\obsidian\Obsidian.exe"
+cmd /c start "" "%LOCALAPPDATA%\Programs\obsidian\Obsidian.exe"
 
 # Launch UWP Apps (WhatsApp)
 explorer.exe shell:appsFolder\5319275A.WhatsAppDesktop_cv1g1gvanyjgm!App
