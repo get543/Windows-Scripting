36e153ae1f3404f04b90210794a3847fbfb6eb7f
get543 <fghaza.pratama@gmail.com>
2026-03-31 11:15:41 +0700

    oh-my-posh init command fix

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -341,7 +341,7 @@ function discord() {
 #######################################################################################################################
 # Import PowerShell Theme from oh-my-posh
 # More Themes : https://ohmypo.sh/docs/themes
-oh-my-posh init pwsh --config $env:POSH_THEMES_PATH/takuya.omp.json | Invoke-Expression
+oh-my-posh init pwsh --config takuya | Invoke-Expression
 
 if (!(Get-Module -ListAvailable -Name Terminal-Icons -ErrorAction SilentlyContinue)) {
     Install-Module -Name Terminal-Icons -Scope CurrentUser -Force -SkipPublisherCheck
